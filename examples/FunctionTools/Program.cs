e85feed4936416ce294504c61d6d70f1a31e9729
automaze1 <138268276+automaze1@users.noreply.github.com>
2023-08-27 12:46:40 +0800

    Update class diagram and sample code in example project

--- examples/FunctionTools/Program.cs
+++ examples/FunctionTools/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Automation.GenerativeAI;
+using Automation.GenerativeAI.Chat;
 using Automation.GenerativeAI.Interfaces;
+using Automation.GenerativeAI.LLM;
 using Automation.GenerativeAI.Tools;
 using System;
 using System.Collections.Generic;
@@ -24,26 +26,182 @@ namespace FunctionTools
 
         static async Task Main(string[] args)
         {
+            //await DemoBasicConversationUsingLanguageModel();
+
+            //await DemoConversationWithOpenAIModel();
+
+            //await DemoFunctionCalling();
+
+            //await DemoPromptAndQueryTool();
+
+            //await DemoPipeline();
+
+            //await DemoMapReduce();
+
+            await DemoSearchTool();
+
             //await SimpleChat();
             //await LatestNews();
 
             //await BrowsePage(@"https://www.google.com/finance/?hl=en");
 
             //await ExtractData();
-
-            await MapReduce();
         }
 
-        static async Task MapReduce()
+        static async Task DemoBasicConversationUsingLanguageModel()
         {
-            var prompt = QueryTool.WithPromptTemplate("What is the capitol of {{$state}} and which language is spoken there?");
+            //Create OpenAI Language model
+            var apikey = ""; //Pass your API key, or set your api key to OPENAI_API_KEY environment variable
+            var llm = new OpenAILanguageModel("gpt-3.5-turbo-0613", apikey);
 
+            //Create Chat Message with a specific role
+            var message = new ChatMessage(Role.user, "Hi, there!! I am Ram");
+
+            //Get a Response from the language model using a list of chat messages.
+            var response = await llm.GetResponseAsync(Enumerable.Repeat(message, 1), 0.8);
+            //response.Response provides the response message
+            Console.WriteLine(response.Response);
+        }
+
+        static async Task DemoConversationWithOpenAIModel()
+        {
+            //Create GenAI service
+            var service = Application.GetAIService();
+
+            var apikey = ""; //Pass your API key, or set your api key to OPENAI_API_KEY environment variable
+
+            //Create Language Model
+            var llm = service.CreateOpenAIModel("gpt-3.5-turbo-0613", apikey);
+
+            //Create Conversation
+            var chat = service.CreateConversation("Test", llm);
+            chat.AppendMessage("Hi, there!! I am Ram", Role.user);
+            var response = await chat.GetResponseAsync(0.8);
+
+            Console.WriteLine($"{response.role}: {response.content}");
+        }
+
+        static async Task DemoFunctionCalling()
+        {
+            //Define function descriptor
+            var p1 = new ParameterDescriptor()
+            {
+                Name = "location",
+                Description = "The city and state, e.g. San Francisco, CA",
+            };
+            var p2 = new ParameterDescriptor()
+            {
+                Name = "unit",
+                Description = string.Empty,
+                Required = false,
+                Type = new EnumTypeDescriptor(new string[] { "celsius", "fahrenheit" }),
+            };
+            var parameters = new List<ParameterDescriptor>() { p1, p2 };
+
+            var name = "get_current_weather";
+            var description = "Get the current weather in a given location";
+
+            var function = new FunctionDescriptor(name, description, parameters);
+            
+            //Create OpenAI Language model
+            var apikey = ""; //Pass your API key, or set your api key to OPENAI_API_KEY environment variable
+            var llm = new OpenAILanguageModel("gpt-3.5-turbo-0613", apikey);
+
+            //Create user message
+            var message = new ChatMessage(Role.user, "What is the weather like in Boston?");
+
+            //Get a response from the LLM, it must return a function call message.
+            var response = await llm.GetResponseAsync(
+                            Enumerable.Repeat(message, 1),
+                            Enumerable.Repeat(function, 1),
+                            0.8);
+            //response.Type == Response.Type.FunctionCall
+            //response.Response is a JSON object to provide function details
+            Console.WriteLine($"Response Type: {response.Type}");
+            Console.WriteLine($"Response Message: {response.Response}");
+        }
+
+        static async Task DemoPromptAndQueryTool()
+        {
+            //Define a template string, the template variables can be denoted as {{ $input }}
+            //Following template has two variable adjective and content.
+            var template = @"Tell me a {{ $adjective }} joke about {{ $content }}.";
+
+            //Create PromptTemplate with the template string
+            var prompt = new PromptTemplate(template, Role.user);
+
+            //Define execution context with the parameter values
+            var context = new ExecutionContext();
+            context["adjective"] = "funny";
+            context["content"] = "chicken";
+
+            //You can format the message to update the variables in the template string to get the message
+            var msg = prompt.FormatMessage(context); //msg.content == "Tell me a funny joke about chicken."
+
+            Console.WriteLine($"{msg.role}: {msg.content}");
+
+            //Create OpenAI Language model
+            var apikey = ""; //Pass your API key, or set your api key to OPENAI_API_KEY environment variable
+            var llm = new OpenAILanguageModel("gpt-3.5-turbo-0613", apikey);
+
+            //Create a QueryTool to process this template with the language model
+            var tool = QueryTool.WithPromptTemplate(prompt)
+                                .WithLanguageModel(llm);
+
+            var result = await tool.ExecuteAsync(context); //Returns a funny joke about chicken
+
+            Console.WriteLine($"Joke: {result}");
+        }
+
+        static async Task DemoPipeline()
+        {
+            //Create OpenAI Language model
+            var apikey = ""; //Pass your API key, or set your api key to OPENAI_API_KEY environment variable
+            var llm = new OpenAILanguageModel("gpt-3.5-turbo-0613", apikey);
+
+            //Define a couple of template strings
+            var template1 = "Provide me an engaging title for a blog on topic '{{$topic}}' for '{{$audience}}'.";
+            var template2 = "Give me outline of a blog with title: {{$title}}.";
+
+            //Create QueryTool with these prompt templates
+            var tools = new[]
+            {
+                QueryTool.WithPromptTemplate(template1).WithLanguageModel(llm).WithName("Tool_1"),
+                QueryTool.WithPromptTemplate(template2).WithLanguageModel(llm).WithName("Tool_2"),
+            };
+
+            //create pipeline with tools
+            var pipeline = Pipeline.WithTools(tools);
+
+            //Define execution context with the parameter values
+            var context = new ExecutionContext();
+            context["topic"] = "Generative AI";
+            context["audience"] = "Children";
+
+            var outline = await pipeline.ExecuteAsync(context); //returns the complete outline for the Blog
+
+            //Get the title of the blog from the context
+            object title;
+            context.TryGetResult(tools.First().Name, out title);
+
+            Console.WriteLine($"TITLE: {title}");
+            Console.WriteLine();
+            Console.WriteLine(outline);
+        }
+
+        static async Task DemoMapReduce()
+        {
+            //Prompt tool that takes 3 input variables to create a sentence.
+            var prompt = PromptTool.WithTemplate("The capital of {{$state}} is {{$city}} and '{{$language}}' is the most popular language there.");
+
+            //Combines a given array of text by joining the text with a new line.
             var combine = new CombineTool();
-
+            //Create a MapReduce tool
             var mapreduce = MapReduceTool.WithMapperReducer(prompt, combine);
 
             var context = new ExecutionContext();
 
+            //The context has an array of data for each input variable from the mapper tool.
             context["state"] = new[] { "UP", "Bihar", "Jharkhand", "MP" };
             context["city"] = new[] { "Lucknow", "Patna", "Ranchi", "Bhopal" };
             context["language"] = new[] { "Hindi", "Bhojpuri", "Santhal", "Hindi" };
@@ -52,6 +210,23 @@ namespace FunctionTools
 
             Console.WriteLine(result);
             Console.ReadLine();
+        }
+
+        static async Task DemoSearchTool()
+        {
+            //Get Bing API key
+            string apiKey = "";
+
+            //Create Bing Search tool with max result count 5
+            var tool = SearchTool.ForBingSearch(apiKey).WithMaxResultCount(5);
+
+            var context = new ExecutionContext();
+            context[tool.Descriptor.InputParameters.First()] = "What is the latest update on India's moon mission?";
+
+            //Execute the search query
+            var result = await tool.ExecuteAsync(context);
+
+            Console.WriteLine(result);
         }
 
         static async Task ExtractData()
